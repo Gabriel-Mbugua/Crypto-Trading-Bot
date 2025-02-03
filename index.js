c99f5efb016a58a5a8fc8bc84efbd647e7f7499e
Gabriel Mbugua <gabriel.mbugua307@gmail.com>
2025-02-04 01:11:58 +0300

    Fixing port issue

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello World" });
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
 });
